5861fa726c4b12799dbb303b43c03b34a2436d0f
JMGella <151771028+JMGella@users.noreply.github.com>
2025-01-17 12:12:28 +0100

    añadido categoria

---
+++ src/main/java/org/example/apitiendaaa/domain/Category.java
@@ -0,0 +1,28 @@
+package org.example.apitiendaaa.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column
+    private LocalDate creationDate;
+    @Column
+    private boolean active;
+    @Column
+    private String image;
+}
