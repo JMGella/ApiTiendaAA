eee56b23b7dbc121c11d8ec30a4b0317fb261623
JMGella <151771028+JMGella@users.noreply.github.com>
2025-05-19 12:40:53 +0200

    terminado userservicetest, creado categoryservicetest

--- src/test/java/org/example/apitiendaaa/UserServiceTests.java
+++ src/test/java/org/example/apitiendaaa/UserServiceTests.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -74,6 +75,29 @@ public class UserServiceTests {
     }
 
     @Test
+    public void testGetAllEmail(){
+        List<User> userlist = List.of(new User(1, "Juan", "email", null, true, "address", "phone", null, "latitude", "longitude", null)
+        );
+
+        List<UserOutDTO> userOutDTOList = List.of(new UserOutDTO(1, "Juan", "email", null, true, "address", "phone",null, "latitude", "longitude")
+        );
+
+        when(userRepository.findByEmail("email")).thenReturn(userlist);
+        when(modelMapper.map(userlist, new TypeToken<List<UserOutDTO>>() {}.getType())).thenReturn(userOutDTOList);
+
+        userService.getAll("", "email", false);
+
+        assertEquals(1, userlist.size());
+        assertEquals(1, userOutDTOList.size());
+        assertEquals("Juan", userlist.get(0).getName());
+
+
+        verify(userRepository,times(1)).findByEmail("email");
+    }
+
+
+
+    @Test
     public void testGet() throws UserNotFoundException {
         User user = new User(1, "Juan", "email", null, true, "address", "phone", null, "latitude", "longitude", null);
 
@@ -90,6 +114,16 @@ public class UserServiceTests {
     }
 
     @Test
+    public void testGetNotFound() {
+        when(userRepository.findById(99L)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> {
+            userService.get(99L);
+        });
+    }
+
+
+    @Test
     public void testAdd(){
         User user = new User(1, "Juan", "email", null, true, "address", "phone", null, "latitude", "longitude", null);
 
@@ -102,4 +136,54 @@ public class UserServiceTests {
 
         verify(userRepository,times(1)).save(user);
     }
+
+    @Test
+    public void testUpdate() throws UserNotFoundException {
+        User existingUser = new User(1, "Juan", "email", null, true, "address", "phone", null, "latitude", "longitude", null);
+        User updatedData =  new User(1, "John", "new email", null, true, "new address", "123", null, "latitude", "longitude", null);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
+        when(userRepository.save(updatedData)).thenReturn(updatedData);
+
+        User result = userService.update(1L, updatedData);
+
+        assertEquals("John", result.getName());
+        assertEquals("new email", result.getEmail());
+        assertEquals("new address", result.getAddress());
+        assertEquals("123", result.getPhone());
+
+        verify(userRepository).save(existingUser);
+        verify(userRepository, times(1)).findById(1L);
+        verify(userRepository, times(1)).save(existingUser);
+    }
+
+    @Test
+    public void testUpdate_UserNotFound() {
+        User user = new User();
+
+        when(userRepository.findById(123L)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userService.update(123L, user));
+    }
+
+    @Test
+    public void testDelete() throws UserNotFoundException {
+        User user = new User();
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        userService.delete(1L);
+
+        verify(userRepository).deleteById(1L);
+        verify(userRepository, times(1)).findById(1L);
+        verify(userRepository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    public void testDelete_UserNotFound() {
+        when(userRepository.findById(123L)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userService.delete(123L));
+    }
+
 }
+
